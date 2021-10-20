e60e62cec48bd09eaf7e3d6b0cb83601839d2e6b
Opilane TTHK <opilane@tthk.ee>
2021-10-20 09:49:57 +0300

    week8Arrays

---
+++ Functions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sisesta oma eesnimi:");
+            string username = Console.ReadLine();
+            Console.WriteLine("sisestage oma perekonnanimi");
+            string userlastname = Console.ReadLine();
+            Console.WriteLine("sisestage oma sünniaasta");
+            int userage = Convert.ToInt32(Console.ReadLine());
+            
+            Greetings(username, userlastname, userage);  
+        }
+
+        private static void Greetings(string someString, string anotherstring, int someint)
+        {
+            Console.WriteLine($"Tere, {someString} {anotherstring}!");
+            Console.WriteLine($"oled {2021 - someint}");
+        }
+    }
+}
---
+++ FunctionsWithArray/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FunctionsWithArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] colors = { "red", "blue", "green" };
+            string[] morecolors = { "yellow", "pink", "beige", "purple" };
+            PickRandomColor(colors);
+            PickRandomColor(morecolors);
+        }
+
+        private static void PickRandomColor(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomindex = rnd.Next(0, someArray.Length);
+
+            Console.WriteLine(someArray[randomindex]);
+        }
+    }
+}
